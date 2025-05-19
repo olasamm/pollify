0dcd8c42a2004aa9fc199c03ea27215cb636be76
olasamm <gbadebosamuel23@gmail.com>
2025-05-19 09:16:24 +0100

    signup and signin done

--- client/component/Navbar.jsx
+++ client/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css';
 import { Link } from 'react-router-dom'
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
@@ -10,16 +11,16 @@ const Navbar = () => {
         <div className="container-fluid">
           <a className="navbar-brand text-light mx-5 polly" href="#">POLLIFY </a>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars text-light"><FaBarsStaggered /></i> 
+        </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ms-auto">
               <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
@@ -28,9 +29,12 @@ const Navbar = () => {
               <a className="nav-link text-light" href="#">Poll</a>
             </div>
             <div className="mx-5 d-flex flex-column flex-lg-row justify-content-md-start">
-            <Link to="/Signin"> <button className="btn btn-primary mx-2 my-1 my-lg-0" type="button">SignIn</button></Link> 
-            <Link to="/Signup">  <button className="btn btn-primary my-1 my-lg-0" type="button">SignUp</button></Link>
-            LINK
+           <Link to="/Signin">
+            <button className="btn btn-primary  my-1 my-lg-0 w-100 w-lg-auto" type="button">SignIn</button>
+          </Link> 
+          <Link to="/Signup">
+            <button className="btn btn-primary my-1 mx-2 my-lg-0 w-100 w-lg-auto" type="button">SignUp</button>
+          </Link>
           </div>
           </div>
         </div>
